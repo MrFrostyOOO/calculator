cd11fdc271cdf1e416d8523cff195617209900ed
Bhuwan Joshi <bhuwanjoshi2062@gmail.com>
2025-01-20 17:31:39 +0545

    changes to js code

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,40 +5,49 @@ function takeInput(userInput) {
     let input = document.getElementById("display");
     equation = equation + userInput;
     input.value = equation.replace(/\*\*/g, "^");
-    // console.log("userInput:"+userInput)
-    // console.log("equation:"+equation)
   }
 }
 
 function calculate() {
+  // store values
   let secDisplay = document.getElementById("secDisplay");
-  secDisplay.value = equation.replace(/\*\*/g, "^");
-  try {
-    if (isNaN(eval(equation))) {
-      equation = "";
-      let input = document.getElementById("display");
-      input.value = "Math Error";
-    } else {
-      let input = document.getElementById("display");
-      equation = eval(equation);
-      equation = equation.toString();
-      if (equation.includes('.') && (equation.split('.')[1].length + equation.split('.')[0].length)>15 ){
-        equation = parseFloat(equation).toFixed(15 - equation.split('.')[0].length);
-        input.value = equation;
-        equation= parseFloat(equation).toFixed(2);
+  let input = document.getElementById("display");
 
-      } else {
+  // store equation to secondary display
+  secDisplay.value = equation.replace(/\*\*/g, "^");
+
+  let answer = eval(equation);
+
+  // for catching errors
+  try {
+    // checking if the answer is a number or not
+    if(equation==""){
+      equation="";
+    }else if (isNaN(answer)) {
+
+      equation = "";
+      input.value = "Math Error";
+
+    } else {
+
+      equation = answer.toString();
+      if (
+        equation.includes(".") &&
+        equation.split(".")[1].length + equation.split(".")[0].length > 15
+      ) {
+        equation = answer.toFixed(
+          15 - equation.split(".")[0].length
+        );
         input.value = equation;
+        equation = answer.toFixed(2);
+      } else {
+        input.value = answer;
       }
-      
-      // console.log(equation);
-      // console.log(equation.length);
     }
   } catch (error) {
     equation = "";
     let input = document.getElementById("display");
     input.value = "Syntax Error";
-    // console.log("Error");
   }
 }
 
