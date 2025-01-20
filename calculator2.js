0b7c907cd1044a2207625189bcce157e51f2cc5c
Bhuwan Joshi <bhuwanjoshi2062@gmail.com>
2025-01-20 21:30:05 +0545

    solved syntax error not displaying for certain combination of characters problem

--- calculator2.js
+++ calculator2.js
@@ -1,43 +1,51 @@
 const display = document.getElementById("display");
 const secDisplay = document.getElementById("secDisplay");
 
-function takeInput(input){
-    if(display.value.length < 15){
-        display.value += input;
-    }
+function takeInput(input) {
+  if (display.value == "Math Error" || display.value == "Syntax Error") {
+    display.value = "";
+  }
+
+  if (display.value.length < 15) {
+    display.value += input;
+  }
 }
 
-function backspace(){
-    display.value = display.value.slice(0,-1);
+function backspace() {
+  display.value = display.value.slice(0, -1);
 }
 
-function clearDisplay(){
- 
-    if (display.value != "" ) {
-        display.value="";
-    } else if(secDisplay != ""){
-        secDisplay.value = "";
-    }
+function clearDisplay() {
+  if (display.value != "") {
+    display.value = "";
+  } else if (secDisplay != "") {
+    secDisplay.value = "";
+  }
 }
 
-function calculate(){
-    try{
-        if(display.value != "Math Error" && display.value != "Syntax Error"){
-            let result = eval(display.value);
-            secDisplay.value = display.value;
+function calculate() {
+  try {
+    if (/\/\/|\*\//.test(display.value)) {
+      display.value = "Syntax Error";
+    } else if (
+      display.value != "Math Error" &&
+      display.value != "Syntax Error"
+    ) {
+      let result = eval(display.value);
+      secDisplay.value = display.value;
 
-            if (isNaN(result) && display.value != "") {
-                display.value = "Math Error";
-            } else if(display.value != ""){
-                result= result.toString();
-                if(result.includes('.') ){
-                    display.value= parseFloat(result).toFixed(2);
-                }else{
-                    display.value = result;
-                }
-            }
+      if (isNaN(result) && display.value != "") {
+        display.value = "Math Error";
+      } else if (display.value != "") {
+        result = result.toString();
+        if (result.includes(".")) {
+          display.value = parseFloat(result).toFixed(2);
+        } else {
+          display.value = result;
         }
-    }catch(Error){
-        display.value = "Syntax Error";
+      }
     }
+  } catch (Error) {
+    display.value = "Syntax Error";
+  }
 }
