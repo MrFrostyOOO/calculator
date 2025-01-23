8e89981e1d40d4128b254b0195f561426d84f8a9
Bhuwan Joshi <bhuwanjoshi2062@gmail.com>
2025-01-23 16:04:47 +0545

    made JS code more cleaner

--- calculator2.js
+++ calculator2.js
@@ -1,59 +1,103 @@
 // Declaring Global variables
-
+const empty = "";
 const display = document.getElementById("display");
 const secDisplay = document.getElementById("secDisplay");
-let bufferInput = "";
+
+let bufferInput = empty;
 
 function takeInput(input) {
-  if (display.value == "Math Error" || display.value == "Syntax Error") {
-    bufferInput= "";
-    display.value = "";
+  if (isDisplayingError()) {
+    bufferInput = empty;
+    display.value = empty;
   }
-  if(display.value.length < 15) {
+  if (display.value.length < 15) {
     bufferInput += input;
     display.value = bufferInput.replace(/\*\*/g, "^");
   }
 }
 
-function backspace() { 
-  display.value = display.value.slice(0, -1);
-  bufferInput = display.value.replace(/\^/g, "**"); //taking the last character from the buffer while making sure no error occurs
-}
-
-function clearDisplay() {
-  if (display.value != "") {
-    bufferInput = "";
-    display.value = "";
-  } else if (secDisplay != "") {
-    secDisplay.value = "";
+function backspace() {
+  if (isDisplayingError()) {
+    bufferInput = empty;
+    display.value = empty;
+  }else{
+    display.value = display.value.slice(0, -1);
+    bufferInput = display.value.replace(/\^/g, "**");
   }
 }
 
-function calculate() {
-  try {
-    if (/\/\/|\*\//.test(bufferInput)) { //checking for syntax error that do not occur with eval function
-      display.value = "Syntax Error";
-    } else if (
-      display.value != "Math Error" &&  //checking that the expression can proceed further
-      display.value != "Syntax Error"
-    ) {
-      let result = eval(bufferInput); //storing the anser in a result variable
-      secDisplay.value = bufferInput.replace(/\*\*/g, "^")+" = "+eval(display.value); //showing expression in secDisplay
+function Cclicked() {
+  if (!displayIsEmpty()) {
+    bufferInput = empty;
+    display.value = empty;
+  } else if (!secDisplayIsEmpty()) {
+    secDisplay.value = empty;
+  }
+}
 
-      if (isNaN(result) && display.value != "") { //checking if the answer can be shown
-        display.value = "Math Error"; //showing math error if it is not a proper answer
-      } else if (display.value != "") { //checking that the expression is not just empty string
-        result = result.toString();
-        if (result.includes(".")) { //checking if the answer is a floating value or not
-          bufferInput = parseFloat(result).toFixed(2); //shortning the answer and displaying if float
-          display.value = bufferInput;
-        } else {
-          bufferInput = result;
-          display.value = result; //displaying the answer if integer
-        }
-      }
+function displayAnswer(result) {
+  result = result.toString();
+
+  if (result.includes(".")) {
+    bufferInput = parseFloat(result).toFixed(2);
+    display.value = bufferInput;
+  } else {
+    bufferInput = result;
+    display.value = result;
+  }
+}
+
+function validate() {
+  try {
+    let result = eval(bufferInput);
+
+    if (/\/\/|\*\//.test(bufferInput)) {
+      displaySyntaxError();
+    } else if ((isNaN(result) || !isFinite(result)) && !displayIsEmpty()) {
+      displayMathError();
+    } else if (!isDisplayingError() && !displayIsEmpty()) {
+      displayEquationInSecDisplay();
+      displayAnswer(result);
     }
-  } catch (Error) {
-    display.value = "Syntax Error"; //showing syntax error if the eval function causes any error
+  } catch (error) {
+    displaySyntaxError();
+  }
+}
+
+function displaySyntaxError() {
+  display.value = "Syntax Error";
+}
+
+function displayMathError() {
+  display.value = "Math Error";
+}
+
+function displayEquationInSecDisplay() {
+  secDisplay.value =
+    bufferInput.replace(/\*\*/g, "^") + " = " + eval(display.value);
+}
+
+function displayIsEmpty() {
+  if (display.value == empty) {
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function secDisplayIsEmpty() {
+  if (secDisplay.value == empty) {
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function isDisplayingError() {
+  if (display.value == "Math Error" || display.value == "Syntax Error") {
+    return true;
+  }
+  else{
+    return false;
   }
 }
